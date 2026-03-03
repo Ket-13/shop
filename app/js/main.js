d004db02a88b10011088a0f327d6691d7ca01570
EkaterinaRyshkanichgit config --global user.emai ekaterinaryskanic@gmail.comgit config --global core.autocrlf truegit config --global core.safecrlf warngit initis -agit status git config --global user.email ekaterinaryskanic@gmail.com <@gmail.com>
2026-03-03 16:49:39 +0200

    Add dist folder

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,111 @@
+const openBtn = document.querySelector('.open-modal');
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+function openModal() {
+  modal.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closeModal() {
+  modal.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (modalOverlay) {
+  modalOverlay.addEventListener('click', closeModal)
+}
+
+openModal.addEventListener('click, openModal');
+closeModal.addEventListener('click, closeModal');
+
+document.addEventListener('keydown', (e) => {
+  if(e.key = 'Escape' && modal.getAttribute('aria-hidden') === 'false') {
+    closeModal()
+  }
+})
+
+const openPopupBtn = document.querySelector('.catalog-popup');
+ const popup = document.querySelector('.popup');
+const popupOverlay = document.querySelector('.popup-overlay');
+const closePopupBtn = document.querySelector('.close-popup');
+
+function openPopup() {
+  popup.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closePopup() {
+  popup.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (popupOverlay) {
+  popupOverlay.addEventListener('click', closePopup)
+}
+
+openPopupBtn.addEventListener('click, openPopup');
+closePopupBtn.addEventListener('click, closePopup');
+
+document.addEventListener('keydown', (e) => {
+  if(e.key = 'Escape' && popup.getAttribute('aria-hidden') === 'false') {
+    closePopup()
+  }
+})  
+
+
+const breakpoint = window.matchMedia('(min-width:600px)');
+let sliderMobile = null;
+
+function initSwiper() {
+  sliderMobile  = new Swiper(".slider-mobile", {
+  loop: true,
+  slidesPerView: 1,
+  
+
+  navigation: {
+    nextEl: ".product__arrow-next",
+    prevEl: ".product__arrow-prev",
+  },
+});
+}
+
+function destroySwiper() {
+  if (sliderMobile) {
+    sliderMobile.destroy(true, true);
+    sliderMobile = null;
+  } 
+}
+
+function handleBreakpointChange(e) {
+  if (e.matches) {
+    destroySwiper()
+  } else {
+   if (sliderMobile) initSwiper();
+  }
+}
+
+handleBreakpointChange(breakpoint);
+
+breakpoint.addEventListener('change', handleBreakpointChange);
+
+const headerBtn = document.querySelector('.header__btn');
+const menu = document.querySelector('.menu');
+
+headerBtn.addEventListener('click', ()=> {
+  menu.classList.toggle('menu--active');
+});
 const modeContainer = document.querySelector(".view-mode__container");
 const modeBtnGrid = document.querySelector("view-mode__btn-grid");
 const modeBtnLine = document.querySelector("view-mode__btn-line");
 
-modeBtnGrid.addEventListener("click", () => {
+modeBtnGrid?.addEventListener("click", () => {
   modeContainer.classList.add("view-mode__container--grid");
   modeContainer.classList.remove("view-mode__container--line");
 });
 
-modeBtnLine.addEventListener("click", () => {
+modeBtnLine?.addEventListener("click", () => {
   modeContainer.classList.remove("view-mode__container--grid");
   modeContainer.classList.add("view-mode__container--line");
 });
@@ -21,6 +119,22 @@ const swiper = new Swiper(".accessories__slider", {
   navigation: {
     nextEl: ".accessories__arrow-next",
     prevEl: ".accessories__arrow-prev",
+  },
+
+   breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+      /* centeredSlider:true, */
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
 });
 
@@ -37,6 +151,21 @@ const swiperReviews = new Swiper(".reviews__slider", {
   pagination: {
     el: ".reviews__pagination",
     type: "fraction",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 6,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 8,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 12,
+      spaceBetween: 16,
+    },
   },
 });
 
